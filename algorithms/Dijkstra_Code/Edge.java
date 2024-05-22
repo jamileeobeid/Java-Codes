1c100b88ddd2c6249d54477de8045db38bf1da77
Jamilé Obeid <jamileemhd@gmail.com>
2024-05-22 21:39:28 +0400

    Dijkstra Codes

---
+++ algorithms/Dijkstra_Code/Edge.java
@@ -0,0 +1,39 @@
+package W6_Deneme;
+public class Edge<E> implements Comparable<Edge<E>> {
+    private double weight;
+    private WVertex<E> from;
+    private WVertex<E> to;
+
+    public Edge(WVertex<E> from, WVertex<E> to, double weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+    public WVertex<E> getFrom() {
+        return from;
+    }
+
+    public WVertex<E> getTo() {
+        return to;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{"
+            + "from=" + from.getValue()
+            + ", to=" + to.getValue()
+            + ", weight=" + this.weight
+            + "}";
+    }
+
+    @Override
+    public int compareTo(Edge<E> o) {
+        // can't return 0 so use <=
+        return this.weight <= o.weight ? -1 : 1;
+    }
+}
+
---
+++ algorithms/Dijkstra_Code/WGraph.java
@@ -0,0 +1,12 @@
+package W6_Deneme;
+public interface WGraph<E> {
+    void add(E value);
+    boolean contains(E value);
+    int size();
+    void connect(E a, E b, double weight);
+    boolean connected(E a, E b);
+    //it will return the weight of that edge
+    double weight(E a, E b);
+   
+
+}
